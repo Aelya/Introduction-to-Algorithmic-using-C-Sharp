b1724668b98c612740bd6d4d3872867b230d6a4c
Unknown <elya.rezzoug@gmail.com>
2017-11-15 19:42:38 +0100

    Changing indentation

--- VarAbsolute.cs
+++ VarAbsolute.cs
@@ -1,17 +1,18 @@
 using System;
 
-namespace CsExosAlgo1
- {
-  class ApplicationValAbsolue1
-   {
-static void Main(string[ ] args)
- {
-    double x;
+namespace VarAbsolute
+{
+    class ApplicationVarAbsolute
+    {
+        static void Main(string[] args)
+        {
+            double x;
 
-System.Console.Write("Entrez un nombre x = ");
-x = Double.Parse( System.Console.ReadLine( ) ) ;
-if (x<0) System.Console.WriteLine("|x| = "+(-x));
-else System.Console.WriteLine("|x| = "+x);
-  }
- }
+            System.Console.Write("Entrer un nombre x = ");
+            x = Double.Parse(System.Console.ReadLine());
+            if (x < 0) System.Console.WriteLine("|x| = " + (-x));
+            else System.Console.WriteLine(" |x| = " + x);
+            
+        }
+    }
 }
